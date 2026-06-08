1a4d8d28dac61e2aec07f6c12d4e7bbd8d7e8166
Ankush <ankush.malode@vedam.org>
2026-06-08 06:26:02 +0000

    active.rb file update

--- db/languages/active.rb
+++ db/languages/active.rb
@@ -226,10 +226,10 @@
   {
     id: 71,
     name: "Python (3.13.5)",
-    run_cmd: "/usr/bin/python3 script.py",
     #name: "Python (3.8.1)",
     is_archived: false,
-    source_file: "script.py"
+    source_file: "script.py",
+    run_cmd: "/usr/bin/python3 script.py"
     #run_cmd: "/usr/local/python-3.8.1/bin/python3 script.py"
   },
   {
